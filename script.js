d68570e623dc3ecce6a0b730a132d095f2fd5f91
misikira <misikirteka@gmail.com>
2023-07-06 14:20:33 +0300

    ajac call made

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getcdata=function(country){
 const request=new XMLHttpRequest;
-request.open('GET','https://restcountries.com/v3.1/name/ethiopia')
+request.open('GET','https://restcountries.com/v2.1/name/${country}')
 request.send();
  request.addEventListener('load',function(){
-    const data=JSON.parse(this.responseText);
+    const [data]=JSON.parse(this.responseText);
     console.log(data)
- })
+
+ const html=`
+ <article class="country">
+ <img class="country__img" src="${data.flag}" />
+ <div class="country__data">
+   <h3 class="country__name">${data.name}</h3>
+   <h4 class="country__region">${data.region}</h4>
+   <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+   <p class="country__row"><span>🗣️</span>${data.languages}</p>
+   <p class="country__row"><span>💰</span>${data.currencies}</p>
+ </div>
+</article>;`
+countriesContainer.insertAdjacentHTML('beforeend',html)
+countriesContainer.style.opacity=1;})}
+getcdata('ethiopia')
+getcdata('usa')
